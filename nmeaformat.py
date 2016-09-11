7cf8b30df76af873dc98d70d39c3ac1d094d5cca
Lasse Karstensen <lasse.karstensen@gmail.com>
2016-09-11 13:32:51 +0200

    Produce correct checksum.

--- nmeaformat.py
+++ nmeaformat.py
@@ -37,7 +37,7 @@ import operator
 
 def checksum(sentence):
     cksum = reduce(operator.xor, (ord(s) for s in sentence[1:-1]))
-    return "%s%s" % (sentence, hex(cksum)[2:4].upper())
+    return "%s%02X" % (sentence, cksum)
 
 def main():
     gpstime = None
