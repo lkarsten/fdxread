02c841901879ade745e4d12540b2883c39faaf86
Lasse Karstensen <lasse.karstensen@gmail.com>
2016-09-11 13:53:25 +0200

    Fix pep8/pylint lint.

--- nmeaformat.py
+++ nmeaformat.py
@@ -23,21 +23,19 @@ it.
 By feeding this into OpenCPN (via kplex in tcp mode), we
 get some visualization.
 """
-import unittest
-from datetime import datetime
-from pprint import pprint, pformat
-from sys import argv, stdin, stdout, stderr
-
 import json
+from operator import xor
+from datetime import datetime
+from sys import stdin, stdout, stderr
+from pprint import pprint, pformat
 
 from LatLon import LatLon, Latitude, Longitude
-from bitstring import BitArray
 
-import operator
 
 def checksum(sentence):
-    cksum = reduce(operator.xor, (ord(s) for s in sentence[1:-1]))
+    cksum = reduce(xor, (ord(s) for s in sentence[1:-1]))
     return "%s%02X" % (sentence, cksum)
+
 
 def main():
     gpstime = None
@@ -53,7 +51,12 @@ def main():
             continue
 
         # oh yeah!
-        sample = json.loads(line)
+        try:
+            sample = json.loads(line)
+        except ValueError:
+            print >>stderr, "Invalid JSON: %s" % line
+            continue
+
         assert type(sample) == dict
 
         if sample["mdesc"] == "dst200depth":
@@ -62,12 +65,13 @@ def main():
             # $--VHW,x.x,T,x.x,M,x.x,N,x.x,K*hh<CR><LF>
 
             res += [("$SDVHW", "0.0", "T", "0.0", "M",
-                    "%.2f" % sample["stw"], "N", "0.0", "K")]
+                     "%.2f" % sample["stw"], "N", "0.0", "K")]
 
         elif sample["mdesc"] == "gpstime":
             # Will be used later on.
             gpstime = datetime.strptime(sample["utctime"], "%Y-%m-%dT%H:%M:%S")
             continue
+
         elif sample["mdesc"] == "gpspos":
             if gpstime is None:
                 continue
@@ -104,7 +108,6 @@ def main():
             #  $--HDT,x.x,T*hh<CR><LF>
             res += [("$GPHDT", "%.2f" % (sample["cog"]), "T")]
 
-
         elif sample["mdesc"] == "gnd10msg3":
             #  $--MWV,x.x,a,x.x,a*hh<CR><LF>
             res = [("$FVMWV",
@@ -118,24 +121,22 @@ def main():
         elif sample["mdesc"] == "environment":
             # $IIXDR,P,1.02481,B,Barometer*0D
             # $IIXDR,C,19.52,C,TempAir*3D
-            res = [
-                ("$ZZXDR",
-                 "P", "%.5f" % sample["airpressure"],
-                 "B", "Barometer",
-                ),
-                ("$ZZXDR",
-                 "C", "%.5f" % sample["temp_c"],
-                 "C", "TempDir"),
-                ]
-
-        elif sample["mdesc"] in ["dst200depth2", "gnd10msg2", "windmsg3", "wind40s"]:
+            res = [("$ZZXDR",
+                    "P", "%.5f" % sample["airpressure"],
+                    "B", "Barometer"),
+                   ("$ZZXDR",
+                    "C", "%.5f" % sample["temp_c"],
+                    "C", "TempDir")]
             continue
 
         else:
             if sample["mdesc"] in ["emptymsg0", "gpsping", "static1s",
-                                   "windsignal"]:
-                continue
-            print >>stderr, "Unhandled: '%s'" % pformat(sample)
+                                   "windsignal", "dst200depth2",
+                                   "gnd10msg2", "windmsg3", "wind40s"]:
+                # Ignore known no-ops.
+                pass
+            else:
+                print >>stderr, "Unhandled: '%s'" % pformat(sample)
             continue
 
         if type(res) == tuple:
