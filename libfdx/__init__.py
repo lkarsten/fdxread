31340e5f441719acdf0ba7529f567fd4fd82be0d
Lasse Karstensen <lasse.karstensen@gmail.com>
2017-03-05 16:02:12 +0100

    Cleanup.

--- libfdx/__init__.py
+++ libfdx/__init__.py
@@ -2,8 +2,5 @@
 from .interfaces import GND10interface, HEXinterface
 from .decode import FDXDecode, DataError, FailedAssumptionError
 
-# from .dumpreader import dumpreader
-# from .nxbdump import nxbdump
-
 from .formats import format_signalk_delta, format_json
 from .format_nmea import format_NMEA0183
