lkarsten/fdxread

commit cdcab8f21c1838f0368f9f1f90db7b4f96d57202

Stop temperature conversion temporarily.
The <150 degree assert is firing, meaning that we're reading something
incorrectly.

Temperature isn't related to navigation, so I'm keeping it fahrenheit until I
have enough information to find the correct value.

libfdx/format_nmea.py
@@ -142,8 +142,8 @@ class format_NMEA0183(object):
                         "B", "Barometer"),
                        ("$ZZXDR",
                         "C",
-                        "%.2f" % sample["temp_c"],
-                        "C",
+                        "%.2f" % sample["temp_f"],
+                        "F",
                         "TempDir")
                        ]
 

libfdx/decode.py
@@ -451,7 +451,7 @@ def FDXDecode(pdu):
         temp = body[32:40].uintle  # zz
         # These are not right. It is never 41 degrees celcius in Norway ;-)
         keys += [('temp_f', temp)]
-        keys += [('temp_c', fahr2celcius(temp))]
+#        keys += [('temp_c', fahr2celcius(temp))]
 
     elif mtype == 0x1c031f:
         mdesc = "wind40s"

commit 8716045a8503e8f91105cdbde288241cf53f0110

Fix test case broken by cdcab8f.

libfdx/format_nmea.py
@@ -142,8 +142,8 @@ class format_NMEA0183(object):
                         "B", "Barometer"),
                        ("$ZZXDR",
                         "C",
-                        "%.2f" % sample["temp_f"],
-                        "F",
+                        "%.2f" % sample.get("temp_c", 0.0),
+                        "C",
                         "TempDir")
                        ]
 